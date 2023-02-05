8f0bef3c2366883d5bfdbdc6df1379e1e7ca1a3c
ozansulukpinar <ozansulukpinar@hotmail.com>
2023-02-06 01:21:59 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# ChimpanzeeTest
+
+A memory test created with HTML, CSS and JavaScript
+
+## Table of contents
+
+- [General info](#general-info)
+- [Technologies](#technologies)
+- [Status](#status)
+- [Contact](#contact)
+
+## General info
+
+This is a basic test that applied to chimpanzees to see their memory abilities.
+
+## Technologies
+
+Project is created with:
+
+- Javascript
+
+## Status
+
+Project is finished.
+
+## Contact
+
+Created by [@ozansulukpinar](https://github.com/ozansulukpinar) - feel free to contact me!
---
+++ app.js
@@ -0,0 +1,79 @@
+var random;
+var randomArray = [];
+var index = idValue = 0;
+
+drawTable();
+
+function drawTable() {
+    var elements = document.getElementsByTagName('table');
+    var table = elements[0];
+
+    for (var k = 0; k < 10; k++){
+        var tr = document.createElement('tr');
+
+        for (var l = 0; l < 10; l++){
+            var th = document.createElement('th');
+            th.className = 'box';
+            th.id = idValue;
+            tr.appendChild(th);
+            idValue++;
+        }
+
+        table.appendChild(tr);
+    }
+
+    $(".box").text("");
+}
+
+setABox();
+
+$("#StartButton").click(function () { 
+    randomArray.forEach(element => {
+      $('#' + element).css("background-color", "black")
+      $('#' + element).addClass("number")
+      $('#' + element).text("")
+    });
+    
+    $('#main').on('click', '.number', function(){
+        var currentID = $(this).attr('id');
+
+        if (currentID != randomArray[index]) {
+            alert("Ooops! Try again!");
+            location.reload();
+        }
+        else {
+            $('#' + currentID).css("background-color", "white");
+            index++;
+        }
+
+        if (index == 10) {
+            alert("Good memory! Congrulations!");
+            location.reload();
+        }
+    });
+    
+    $("#StartButton").prop('disabled', true);
+});
+
+function setABox(){
+    for(var i = 1; i <= 10; i++){
+    fillTheBox(i);
+    }
+}
+
+function fillTheBox(value){
+  random = Math.floor(Math.random() * 99);  
+  var isItInArray = randomArray.includes(random);
+  
+  if(isItInArray){
+    fillTheBox(value);
+  }
+  else{
+    randomArray.push(random);
+    $("#" + random).text(value);
+  }
+  
+  if(randomArray.length == 10){
+    return;
+  }
+}
