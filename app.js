2b6aec092c7c6ae676d4a4271082c316f7050bef
Mr Roux <XenocodeRCE@users.noreply.github.com>
2023-06-28 13:18:03 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
+var random;
 var randomArray = [];
 var index = idValue = 0;
-var level = 4;
-var strikes = 0;
+var niveau 1;
 
 drawTable();
 
@@ -29,82 +29,53 @@ function drawTable() {
 setABox();
 
 $("#StartButton").click(function () { 
-    randomArray = [];
-    setABox();
-    playSequence();
+    randomArray.forEach(element => {
+      $('#' + element).css("background-color", "black")
+      $('#' + element).addClass("number")
+      $('#' + element).text("")
+    });
+    
+    $('#main').on('click', '.number', function(){
+        var currentID = $(this).attr('id');
+
+        if (currentID != randomArray[index]) {
+            alert("Ooops! Try again!");
+            location.reload();
+        }
+        else {
+            $('#' + currentID).css("background-color", "white");
+            index++;
+        }
+
+        if (index == 10) {
+            alert("Good memory! Congrulations!");
+            niveau++;
+            location.reload();
+        }
+    });
+    
     $("#StartButton").prop('disabled', true);
 });
 
-$('#main').on('click', '.box', function(){
-    var currentID = $(this).attr('id');
-
-    if (currentID != randomArray[index]) {
-        strikes++;
-        if (strikes === 3) {
-            alert("🤕 Perdu ! Fautes: " + strikes);
-            resetGame();
-        } else {
-            alert("😓 Oups! Fautes: " + strikes);
-            index = 0;
-            playSequence();
-        }
-    } else {
-        $('#' + currentID).css("background-color", "white");
-        index++;
-
-        if (index === randomArray.length) {
-            level++;
-            alert("😁 Pas mal ! Niveau: " + level);
-            index = 0;
-            playSequence();
-        }
-    }
-});
-
 function setABox(){
-    for(var i = 1; i <= 10; i++){
-        fillTheBox(i);
+    for(var i = 1; i <= niveau; i++){
+    fillTheBox(i);
     }
 }
 
 function fillTheBox(value){
-    var random = Math.floor(Math.random() * 100);  
-    var isItInArray = randomArray.includes(random);
+  random = Math.floor(Math.random() * 99);  
+  var isItInArray = randomArray.includes(random);
   
-    if(isItInArray){
-        fillTheBox(value);
-    }
-    else{
-        randomArray.push(random);
-        $("#" + random).text(value);
-    }
+  if(isItInArray){
+    fillTheBox(value);
+  }
+  else{
+    randomArray.push(random);
+    $("#" + random).text(value);
+  }
   
-    if(randomArray.length === level){
-        return;
-    }
-}
-
-function playSequence() {
-    randomArray.forEach(element => {
-        $('#' + element).css("background-color", "black");
-        $('#' + element).addClass("number");
-        $('#' + element).text("");
-    });
-
-    setTimeout(function () {
-        randomArray.forEach(element => {
-            $('#' + element).css("background-color", "white");
-            $('#' + element).removeClass("number");
-            $('#' + element).text(element);
-        });
-    }, 2000);
-}
-
-function resetGame() {
-    randomArray = [];
-    index = 0;
-    level = 4;
-    strikes = 0;
-    $(".box").text("");
-    $("#StartButton").prop('disabled', false);
+  if(randomArray.length == 10){
+    return;
+  }
 }
